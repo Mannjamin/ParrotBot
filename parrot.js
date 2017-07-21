1c8f61aff626d4f42a7ab43967156a34b73478ae
Ashley Mann <1609144@my.brunel.ac.uk>
2017-07-21 22:49:42 +0100

    Update parrot.js

--- parrot.js
+++ parrot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
+const config = require('./json_files/config.json');
 
 //########################################################################################################
 
