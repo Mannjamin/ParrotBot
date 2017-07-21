74f369d58e1821ff06408a7e34438ab2a60c5ec6
Ashley Mann <1609144@my.brunel.ac.uk>
2017-07-21 15:43:23 +0100

    The machination of pure evil. Handle with care.
    Handle with care.

---
+++ parrot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const client = new Discord.Client();
+
+//########################################################################################################
+
+client.on('ready', () => {
+  	console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+  	client.user.setGame('SQUARK SQUARK!!!!!!')
+});
+
+//########################################################################################################
+
+client.on('message', message => {
+	setTimeout(function()
+	{
+		const embed = {
+			"image": {
+			"url": "https://cdn.discordapp.com/attachments/337938256564060160/337938298393722881/zktRC5t.gif"
+		},
+	};
+	
+	message.guild.channels.find("name", "INSERT_NAME_OF_CHANNEL_HERE").send({embed}) //DO NOT USE THIS BOT WITHOUT GIVING IT A SPECIFIC CHANNEL.
+	
+	}, 30000);
+});
+
+//########################################################################################################
+
+client.login(config.token);
